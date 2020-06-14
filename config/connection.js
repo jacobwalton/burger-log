4994e84e175786601ce057f1797fd35f5a0d83c6
Jacob Walton <jawalton6616@gmail.com>
2020-06-14 15:23:52 -0500

    fixing heroku deployment code

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 var connection;
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(precess.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   const connection = mysql.createConnection({
     host: "localhost",
